f503d97100160130a53cd61b68d423b1b358b293
inexts1996 <inexts0618@gmail.com>
2023-05-16 08:13:09 +0800

    learn mesh basics

--- MeshBasics/Assets/Scripts/Grid.cs
+++ MeshBasics/Assets/Scripts/Grid.cs
@@ -1,30 +1,18 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class Grid : MonoBehaviour
 {
-    [SerializeField]
-    private int _xSize, _ySize;
-
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        Generate();
+        
     }
 
-    private Vector3[] _vertices;
-    private void Generate()
+    // Update is called once per frame
+    void Update()
     {
-        int veticeCount = (_xSize + 1) * (_ySize + 1);
-        _vertices = new Vector3[veticeCount];
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-        foreach (var vertex in _vertices)
-        {
-            Gizmos.DrawSphere(vertex, 0.1f);
-        }
+        
     }
 }
