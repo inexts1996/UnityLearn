855e03fc9bb0d82adfea998909e8a47cc5f02ba8
zhouzigui <zhouzigui@huixuanjiasu.com>
2023-09-04 19:13:19 +0800

    Procedural Grid Programming Form finish

--- MeshBasics/Assets/Scripts/Grid.cs
+++ MeshBasics/Assets/Scripts/Grid.cs
@@ -9,28 +9,47 @@ public class Grid : MonoBehaviour
 
     private void Awake()
     {
-        StartCoroutine(Generate());
+        Generate();
     }
 
-    private IEnumerator Generate()
-    {
-        var wait = new WaitForSeconds(0.5f);
-        _vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+    private Mesh mesh;
 
+    private void Generate()
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+        _vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        Vector2[] uv = new Vector2[_vertices.Length];
+        Vector4[] tangents = new Vector4[_vertices.Length];
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
         for (int i = 0, y = 0; y <= ySize; y++)
         {
             for (var x = 0; x <= xSize; x++, i++)
             {
                 _vertices[i] = new Vector3(x, y);
-                yield return wait;
+                uv[i] = new Vector2(x * 1f / xSize, y * 1f / ySize);
+                tangents[i] = tangent;
             }
         }
-    }
 
-    private void OnDrawGizmos()
-    {
-        if (_vertices == null) return;
-        Gizmos.color = Color.black;
-        for (var i = 0; i < _vertices.Length; i++) Gizmos.DrawSphere(_vertices[i], 0.1f);
+        mesh.vertices = _vertices;
+        mesh.uv = uv;
+        mesh.tangents = tangents;
+
+        int[] triangles = new int[xSize * ySize * 6];
+
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 2] = triangles[ti + 3] = vi + 1;
+                triangles[ti + 1] = triangles[ti + 4] = xSize + 1 + vi;
+                triangles[ti + 5] = xSize + vi + 2;
+            }
+        }
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
     }
 }
