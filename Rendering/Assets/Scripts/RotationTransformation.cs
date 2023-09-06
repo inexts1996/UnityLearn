649bb0e6509d305095604196b12be2ef5df4b872
zhouzigui <zhouzigui@huixuanjiasu.com>
2023-09-06 22:12:05 +0800

    rendering 1

---
+++ Rendering/Assets/Scripts/RotationTransformation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class RotationTransformation : Transformation
+    {
+        public Vector3 rotation;
+        public override Vector3 Apply(Vector3 point)
+        {
+            var radZ = rotation.z * Mathf.Deg2Rad;
+            float sinZ = Mathf.Sin(radZ);
+            float cosZ = Mathf.Cos(radZ);
+
+            return new Vector3(point.x * cosZ - point.y * sinZ, point.x * sinZ + point.y * cosZ, point.z);
+        }
+    }
+}
---
+++ Rendering/Assets/Scripts/Transformation.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public abstract class Transformation : MonoBehaviour
+    {
+        public abstract Vector3 Apply(Vector3 point);
+    }
+}
